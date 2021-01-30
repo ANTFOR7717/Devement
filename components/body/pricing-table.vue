e7f217acbaa7594236be26f8c445ac6e48ae2d09
ANTFOR7717 <anthonyforest21@gmail.com>
2021-01-30 00:37:46 -0500

    added programmatic navigation

--- components/body/pricing-table.vue
+++ components/body/pricing-table.vue
@@ -84,7 +84,7 @@
 
       <div class="px-4 py-6 pt-6 bg-gray-50 border-t-2 border-gray-200 sm:px-10">
         <div class="mb-2.5">
-          <button @click="this.$router.push('signup')" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button @click="toCreate()" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Create your account
           </button>
         </div>
@@ -137,6 +137,10 @@ export default {
       this.button.premium = true
 
       this.features.current = this.features.premium
+    },
+
+    toCreate(){
+      this.$router.push('signup')
     },
 
 

9967cd8fee0cb68c784b580681d7913682972265
ANTFOR7717 <anthonyforest21@gmail.com>
2021-02-04 16:41:10 -0500

    added products page

---
+++ pages/products.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div>
+     <!-- Background SVG
+    <background-svg/>
+     Container -->
+    <div class="div-container">
+      <!-- Navbar -->
+     <navbar class="mb-10"/>
+      <!-- Main Body -->
+      <products class="body"/>
+
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style scoped>
+
+.body {
+  zoom: 72%
+}
+
+
+.div-container {
+  @apply relative pt-6 mb-20 pb-16 sm:pb-24;
+}
+
+
+</style>
